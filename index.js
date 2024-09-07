34c1ec0137b759da58b089f1770beed5965f7e31
cmerk2021 <techtutorialsmrtech@gmail.com>
2024-09-07 10:58:34 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+module.exports = {
+    stringUtils: {
+        capitalize,
+        decapitalize,
+        // ... other string functions
+    },
+    arrayUtils: {
+        flatten,
+        groupBy,
+        // ... other array functions
+    },
+    // ... other modules
+};
