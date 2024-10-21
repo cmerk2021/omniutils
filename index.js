da4977bdfbf12c2d5877f9242f1d1a0f05f92514
cmerk2021 <76914888+cmerk2021@users.noreply.github.com>
2024-10-20 20:33:53 -0600

    fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import HttpClient from './modules/http.js';
 import * as dataValidation from './modules/validation.js';
 import * as colorUtils from './modules/color.js';
 import Timer from './modules/timer.js'
+import * as RunService from './modules/runservice.js'
 
 export {
     stringUtils,
@@ -16,4 +17,5 @@ export {
     dataValidation,
     colorUtils,
     Timer,
+    RunService,
 };
